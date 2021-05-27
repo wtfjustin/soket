405dd4c535f147282e4fca854dc4415b41c521e2
unknown <wtfjustin>
2021-05-28 02:26:36 +0700

    fix

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 
-const io = require('socket.io')(server)
+const io = require('socket.io')(server,{
+    cors:{
+        origin:"http://localhost"
+    }
+})
 
 
 io.on('connection',(socket)=>{
