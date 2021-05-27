44c84d897296b9d66275eed86d88f80059cf3ce0
unknown <wtfjustin>
2021-05-28 02:15:12 +0700

    fix

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ const server = require('http').createServer(app);
 
 const io = require('socket.io')(server,{
     cors:{
-        origin:"*",
-        credentials:true
+        origin:"*"
     }
 })
 
